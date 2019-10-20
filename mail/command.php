33401d9bb8de23414931e25b617081b243810781
yassinesheasy <yassinesheasy@gmail.com>
2019-10-20 12:22:57 +0200

    add code postal

--- mail/command.php
+++ mail/command.php
@@ -19,29 +19,27 @@ $email = strip_tags(htmlspecialchars($_POST['email']));
 $vehiclerestitution = strip_tags(htmlspecialchars($_POST['vehiclerestitution']));
 
 $selectedservice = strip_tags(htmlspecialchars($_POST['selectedservice']));
-$selecteddateservice = strip_tags(htmlspecialchars($_POST['dateservice']));
-$selectedtimeservice = strip_tags(htmlspecialchars($_POST['timeservice']));
+//$selecteddateservice = strip_tags(htmlspecialchars($_POST['dateservice']));
+//$selectedtimeservice = strip_tags(htmlspecialchars($_POST['timeservice']));
 
 
 
 // Create the email and send the message
 
-$to = "yassinesheasy@gmail.com"; // Add your email address inbetween the "" replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = "haliloho87@gmail.com"; // Add your email address inbetween the "" replacing yourname@yourdomain.com - This is where the form will send a message to.
 
 $subject = "Website Contact Form:";
 
-$body = "You have received a new command .\n\n"."Return of the vehicle : $vehiclerestitution \n\nSelected service : $selectedservice \n\nSelected date service : $selecteddateservice\n\nSelected Time service : $selectedtimeservice";
+$body = "You have received a new command .\n\n"."Return of the vehicle : $vehiclerestitution \n\nSelected service : $selectedservice ";
 
-$header = "From: indexform01@servicesauto.be\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$header = "From: haliloho87@gmail.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 
 $header .= "Reply-To: $email";	
 
 
 
-if(!mail($to, $subject, $body, $header))
+if (!mail($to, $subject, $body, $header)) {
+    http_response_code(500);
+}
 
-  http_response_code(500);
-
-
-?>
 
